22475a6f78b0cfb7819bb64ebcba579fd3491c62
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-22 21:39:04 +0000

    user form + delete btn

--- scraper/models.py
+++ scraper/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Link(models.Model):
 
-    def __str__(self):
-        return self.name
-        
+    # def __str__(self):
+    #     return self.name
+
     address = models.CharField(max_length=1000, null=True, blank=True)
     name = models.CharField(max_length=1000, null=True, blank=True)
--- sitescraper/urls.py
+++ sitescraper/urls.py
@@ -19,5 +19,6 @@ from scraper import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.scrape, name="scrape")
+    path('', views.scrape, name="scrape"),
+    path('delete/', views.clear, name="clear")
 ]
