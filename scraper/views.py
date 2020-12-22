17a5b221ecdc1fa4e27dceead44feba65156d299
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-22 21:20:05 +0000

    site scrape google

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
+
+def scrape(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+    
+    return render(request, 'scraper/result.html', {'link_address':link_address})
